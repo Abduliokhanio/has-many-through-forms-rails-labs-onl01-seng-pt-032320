e7e23e1e03cf1eeccdb39269fb3e41788e3a2eb4
Abdul Khan <akhan17@outlook.com>
2020-08-02 23:53:07 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115202434) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160113001427) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
